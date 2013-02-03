29d2d35c5d5a48e244ca546f4816944b85a24665
Matteo Collina <matteo.collina@gmail.com>
2013-02-03 15:27:03 +0000

    Added support for building custom libpng.
    Closes #1.

--- configs.sh
+++ configs.sh
@@ -1,4 +1,6 @@
 
-export IMAGE_MAGICK_VERSION="6.8.1-10"
+export LIBPNG_VERSION="1.5.14"
+export IMAGE_MAGICK_VERSION="6.8.2-3"
 export REMOTE_PATH="imagemagick-${IMAGE_MAGICK_VERSION}.tgz"
+export LIBPNG_REMOTE_PATH="libpng-${LIBPNG_VERSION}.tgz"
 export S3_BUCKET="buildpack-imagemagick"
