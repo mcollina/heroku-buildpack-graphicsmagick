03fee3bd06bba6cc05053839fa836370f247a21e
Curzola Pierre <pierre.curzola@gmail.com>
2021-09-16 15:32:20 +0200

    chore(compile): remove libraries as they are already included from sources

--- configs.sh
+++ configs.sh
@@ -1,15 +1,4 @@
-
-export ZLIB_VERSION="1.2.9"
-export LIBPNG_VERSION="1.5.17"
-export NASM_VERSION="2.14.02"
-export LIBJPEG_TURBO_VERSION="1.3.0"
-export GRAPHICS_MAGICK_VERSION="1.3.23"
-export REMOTE_PATH="graphicsmagick-${GRAPHICS_MAGICK_VERSION}.tgz"
-export LIBPNG_REMOTE_PATH="libpng-${LIBPNG_VERSION}.tgz"
-export LIBJPEG_TURBO_REMOTE_PATH="libjpeg_turbo-${LIBJPEG_TURBO_VERSION}.tgz"
-export NASM_REMOTE_PATH="nasm-${NASM_VERSION}.tgz"
-export ZLIB_REMOTE_PATH="zlib-${ZLIB_VERSION}.tgz"
-# heroku dropped support for cedar, but leaving for if/when they introduce a
-# new stack
-export STACK="${STACK:-scalingo}"
-export S3_BUCKET="graphicsmagick-buildpack"
+export GRAPHICS_MAGICK_VERSION="1.3.36"
+export REMOTE_PATH="graphicsmagick-${GRAPHICS_MAGICK_VERSION}.tar.gz"
+export STACK="${STACK:-scalingo-18}"
+export S3_BUCKET="buildpacks-repository"
